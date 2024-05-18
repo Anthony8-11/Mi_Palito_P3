1b64985df0145c66eb9ac15592a24f60d2559cf5
Anthony Hernandez <anthonyrhernandez6@gmail.com>
2024-05-18 08:48:37 -0600

    Initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import Arbol.Arbol;
+import ArbolCadena.ArbolCadenas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Arbol arbol = new Arbol();
+//        arbol.insertar(8);
+//        arbol.insertar(9);
+//        arbol.insertar(10);
+//        arbol.insertar(7);
+//        arbol.insertar(2);
+//        arbol.insertar(1);
+
+        ArbolCadenas arbolCadenas = new ArbolCadenas();
+        arbolCadenas.insertar("farore");
+        arbolCadenas.insertar("Impa");
+        arbolCadenas.insertar("Navi");
+        arbolCadenas.insertar("Epoma");
+        arbolCadenas.insertar("Zelda");
+        arbolCadenas.insertar("Anhino");
+    }
+
+
+
 }
